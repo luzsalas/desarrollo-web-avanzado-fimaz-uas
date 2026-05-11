8e77f5998c55db6f2dd7ec3030b4689d3f97d6a0
luzsalas <luzanettesalaspalomares@gmail.com>
2026-05-11 00:50:44 -0700

    se creó el pie de pagina o footer.php

---
+++ parcial-3-mvc/Examen-practico/WEBAPPBASKET/views/admin/template/footer.php
@@ -0,0 +1,48 @@
+ </div>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  </body>
+  <footer class="text-center text-lg-start bg-light text-muted">
+    <section class="p-2">
+      <div class="class-container text-center text-md-start mt-5">
+        <div class="row mt-3">
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <h6>LOGO AQUI</h6>
+
+          </div>
+          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+            <h6 class="text-uppercase fw-bold mb-4">
+              DESARROLLO WEB II
+            </h6>
+            <p>
+              Aplicacion Web con PHP, MySQL, PDO y BootStrap. Clase Desarrollo Web II.
+            </p>
+
+          </div>
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <h6 class="text-uppercase fw-bold mb-4">ENLACES DE INTERÉS</h6>
+            <p>
+              XXXXXXXX.
+            </p>
+
+          </div>
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-4">
+            <h6 class="text-uppercase fw-bold mb-4">CONTACTO</h6>
+            <p>
+                <li class="fw-bold"><i class="fa-solid fa-phone"></i> +526692278028</li>
+                <li class="fw-bold"><i class="fa-solid fa-envelope"></i> luzanettesalaspalomares@gmail.com</li>
+                <li class="fw-bold"><i class="fa-solid fa-school"></i> Facultad de Informática Mazatlán, Universidad Autónoma de Sinaloa.</li>
+            </p>
+
+          </div>
+        </div>
+        <div class="row mt-3">
+          
+        </div>
+      </div>
+    </section>
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.15);">
+      © Clase Desarrollo Web II. MVC. MVC. PHP. MySQL. PDO. BootStrap.
+      
+    </div>
+  </footer>
+</html>
