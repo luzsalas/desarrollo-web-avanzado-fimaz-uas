8765ba242a314fd1eabd6f54b42b85496ddae0fe
luzsalas <luzanettesalaspalomares@gmail.com>
2026-06-24 21:31:45 -0700

    edit.php

--- proyecto-final/TIENDA_MVC/views/productos/edit.php
+++ proyecto-final/TIENDA_MVC/views/productos/edit.php
@@ -2,14 +2,14 @@
 //Proporciona la interfaz de usuario con el formulario para modificar los
 //datos de un producto existente, precargando la información actual mediante 
 //su ID de forma segura para su posterior actualización
-//por: Marysa Quiñonez, Carolina Vazquez, Luz Salas y Mia Rios
 
 require_once __DIR__ . '/../layouts/header.php'; ?>
 
 <h2>Editar producto</h2>
 
-<form action="index.php?route=productos/update" method="POST">
+<form action="/TIENDA_MVC/productos/update" method="POST">
     <input type="hidden" name="id" value="<?= (int)$producto['id']; ?>">
+    <input type="hidden" name="csrf_token" value="<?= $csrfToken ?>">
 
     <div class="mb-3">
         <label class="form-label">SKU</label>
@@ -30,6 +30,7 @@ require_once __DIR__ . '/../layouts/header.php'; ?>
         <label class="form-label">Precio compra</label>
         <input type="number" step="0.01" name="precio_compra" class="form-control" value="<?= htmlspecialchars((string)$producto['precio_compra']); ?>" required>
     </div>
+
     <div class="mb-3">
         <label class="form-label">Precio venta</label>
         <input type="number" step="0.01" name="precio_venta" class="form-control"
@@ -42,7 +43,7 @@ require_once __DIR__ . '/../layouts/header.php'; ?>
     </div>
 
     <button type="submit" class="btn btn-primary">Actualizar</button>
-    <a href="index.php?route=productos" class="btn btn-secondary">Cancelar</a>
+    <a href="/TIENDA_MVC/productos" class="btn btn-secondary">Cancelar</a>
 </form>
 
 <?php require_once __DIR__ . '/../layouts/footer.php'; ?>
